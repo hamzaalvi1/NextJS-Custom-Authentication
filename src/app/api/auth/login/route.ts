8695fed66b44f152637d751451b615f378425ffd
hamza.ahmed@koderlabs.com <hamzaalvi224@gmail.com>
2023-08-01 11:55:01 +0500

    login api developed

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,46 @@
+import { NextResponse, NextRequest } from "next/server";
+import { findUnique } from "@/helpers";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const POST = async (req: NextRequest) => {
+  try {
+    let { email, password } = await req.json();
+
+    const user = findUnique({ email });
+    if (!user) {
+      return NextResponse.json(
+        { message: "Invalid email address" },
+        { status: 400 }
+      );
+    }
+    const isValidPassword = await bcryptjs.compare(password, user.password);
+
+    if (!isValidPassword) {
+      return NextResponse.json({ error: "Invalid password" }, { status: 400 });
+    }
+
+    const token = await jwt.sign(
+      { id: user._id, name: user.name, email: user.email },
+      process.env.TOKEN_SECRET!,
+      { expiresIn: "1d" }
+    );
+
+    // Create a new object without the password property
+    let { password: hashedPassword, ...updatedUser } = user;
+
+    const response = NextResponse.json(
+      { message: "Successfully Logged in", user: updatedUser },
+      { status: 200 }
+    );
+
+    response.cookies.set("access_token", token, { httpOnly: true });
+    return response;
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "Something went wrong" },
+      { status: 200 }
+    );
+  }
+};
