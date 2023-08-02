f2abe0c3421782ddf7183374941a73e4d89819f0
hamza.ahmed@koderlabs.com <hamzaalvi224@gmail.com>
2023-08-02 09:05:01 +0500

    Add middleware file

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const authRegex = /^\/api\/auth\/(.*)$/;
+  
+  if(authRegex.test(request.nextUrl.pathname)){
+    console.log("condition successful");
+  }
+ 
+   
+  
+  //   return NextResponse.redirect(new URL('/home', request.url))
+}
+
+export const config = {
+  matcher: ["/api/auth/:path*"],
+};
